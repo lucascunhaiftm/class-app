aa9345f9f6bb4a901bd71e1a4f462a6d175d85a1
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-03-26 14:29:36 -0300

    Adicionando os includes usados no tamplate principal do produto

---
+++ resources/views/product/partials/header.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+            <li class="dropdown @if (str_contains(Route::current()->getName(), 'products')) active @endif">
+                <a class="dropdown-toggle" data-toggle="dropdown" href={{ route('products.index') }}>Produtos <span
+                        class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href={{ route('products.create') }}>Cadastrar</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+        {{ Route::current()->getName() }}
+    </div>
+</nav>
