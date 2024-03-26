8d98467c1e7eaa89da70a26af005aca0be90b305
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-03-26 16:22:10 -0300

    Ajustando a versão do BS e trocando componente de navbar incorporando aba de user

--- resources/views/product/partials/header.blade.php
+++ resources/views/product/partials/header.blade.php
@@ -1,18 +1,46 @@
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <ul class="nav navbar-nav">
-            <li class="dropdown @if (str_contains(Route::current()->getName(), 'products')) active @endif">
-                <a class="dropdown-toggle" data-toggle="dropdown" href={{ route('products.index') }}>Produtos <span
-                        class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href={{ route('products.index') }}>Listar</a></li>
-                    <li><a href={{ route('products.create') }}>Cadastrar</a></li>
+<nav class="container-fluid navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="dropdown @if (str_contains(Route::current()->getName(), 'products')) active @endif">
+                    <a class="nav-link dropdown-toggle" href={{ route('products.index') }} id="navbarDropdown"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Produtos
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href={{ route('products.index') }}>Listar</a>
+                        <a class="dropdown-item" href={{ route('products.create') }}>Cadastrar</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+            <div class="">
+                <ul class="navbar-nav mr-auto">
+                    <li class="dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href={{ route('profile.edit') }}> {{ __('Profile') }}</a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item" href={{ route('logout') }}
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
+                            </a>
+                        </div>
+                    </li>
                 </ul>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
+            </div>
+        </div>
     </div>
 </nav>
