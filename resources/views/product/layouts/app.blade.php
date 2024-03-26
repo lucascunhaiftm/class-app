8f32aad3ad1b6f1414aa8c89986efc6fde7c0ea1
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-03-26 14:12:15 -0300

    criando template layout principal de produtos

---
+++ resources/views/product/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    @include('product.partials.head')
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            @include('product.partials.header')
+        </div>
+        <div id="main" class="row">
+            @yield('content')
+        </div>
+        <div class="row">
+            @include('product.partials.footer')
+        </div>
+    </div>
+</body>
+</html>
