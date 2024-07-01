5e4b813daab1ccd1dd81b9c9227f2c23592e970e
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-07-01 17:04:01 -0300

    Listagem dos produtos de uma categoria

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -26,5 +26,25 @@
                 </form>
             </div>
         </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Preço</th>
+                    <th scope="col">Estoque</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($category->products as $product)
+                    <tr>
+                        <td scope="row">{{ $product->name }}</td>
+                        <td scope="row">{{ $product->description }}</td>
+                        <td scope="row">{{ $product->price }}</td>
+                        <td scope="row">{{ $product->stock }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
