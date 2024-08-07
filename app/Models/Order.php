0dc6aef1ded3f64c84c8d0b173bc26bf7eb3f891
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-08-07 15:18:13 -0300

    Adição do modelo de pedido

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,9 +19,45 @@ class Order extends Model
     public function products()
     {
         return $this->belongsToMany(Product::class, 'order_product')
-                    ->withPivot('price', 'amount')
-                    ->selectRaw('products.*, order_product.price * amount as total_by_product')
-                    ->as('item');
+            ->withPivot('price', 'amount')
+            ->selectRaw('products.*, order_product.price * amount as total_by_product')
+            ->as('item');
     }
 
+    public function getOrderType()
+    {
+        switch ($this->type) {
+            case "buy":
+                return "Compra";
+                break;
+            case "sell":
+                return "Venda";
+                break;
+        }
+    }
+
+    public function getTotal()
+    {
+        $total= 0;
+        foreach($this->products as $product){
+            $total+=$product->total_by_product;
+        }
+        return $total;
+    }
+
+    public function stockAdjust($data){
+        $items = [];
+        foreach($data['product_id'] as $index=>$product_id){
+            $product = Product::find($product_id);
+            $amount = $data['amount'][$index];
+            if($this->type == Order::BUY){
+                $product->stock += $amount;
+            } else {
+                $product->stock -= $amount;
+            }
+            $product->update();
+            $items[$product->id] = ['price'=> $product->price, 'amount'=> $amount];
+        }
+        return $items;
+    }
 }
